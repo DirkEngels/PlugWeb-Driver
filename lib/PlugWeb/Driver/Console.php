da03d0b0b73d88863743747b1cbc783d7b438a09
Dirk Engels <d.engels@dirkengels.com>
2011-08-17 22:56:01 +0200

    fixed white spaces (coding style)

--- lib/PlugWeb/Driver/Console.php
+++ lib/PlugWeb/Driver/Console.php
@@ -14,112 +14,119 @@ namespace PlugWeb\Driver;
  *
  */
 class Console {
-	
-	protected $_consoleOpts = null;
 
-	/**
-	 * 
-	 * Daemon constructor method
-	 * @param \Zend_Console_Getopt $consoleOpts
-	 */
-	public function __construct(\Zend_Console_Getopt $consoleOpts = null) {
-		$this->setConsoleOpts($consoleOpts);
-	}
-	
-	/**
-	 * 
-	 * Returns an object containing the console arguments.
-	 * @return Zend_Console_Getopt
-	 */
-	public function getConsoleOpts() {
-		// Initialize default console options
-		if (is_null($this->_consoleOpts)) {
-			$this->_consoleOpts = new \Zend_Console_Getopt(
-				array(
-					'mac|m=s' 		=> 'Mac address of plugwise device',
-					'action|a=s'	=> 'Action switch-on, switch-off, info, status',
-					'help|h'		=> 'Show help message (this message)',
-				)
-			);
-		}
-		return $this->_consoleOpts;
-	}
-	
-	/**
-	 * 
-	 * Sets new console arguments
-	 * @param Zend_Console_Getopt $consoleOpts
-	 * @return $this
-	 */
-	public function setConsoleOpts(Zend_Console_Getopt $consoleOpts = null) {
-		if ($consoleOpts === null) {
-			$consoleOpts = $this->getConsoleOpts();
-		}
-		
-		// Parse Options
-		try {
-			$consoleOpts->parse();
-		} catch (Zend_Console_Getopt_Exception $e) {
-			echo $e->getUsageMessage();
-			exit;
-		}
-		$this->_consoleOpts = $consoleOpts;
+    protected $_consoleOpts = null;
 
-		return $this;
-	}
-	
-	
-	public function run() {
-		// Set action
-		$mac = $this->_consoleOpts->getOption('mac');
-		$action = $this->_consoleOpts->getOption('action');
+    /**
+     * 
+     * Daemon constructor method
+     * @param \Zend_Console_Getopt $consoleOpts
+     */
+    public function __construct(\Zend_Console_Getopt $consoleOpts = null) {
+        $this->setConsoleOpts($consoleOpts);
+    }
 
-		$device = new \PlugWeb\Driver\Device($mac);
-		$data = array();
-		switch($action) {
-			case 'switch-on':
-				$data =$device->powerSwitchOn();
-				break;
-			case 'switch-off':
-				$data = $device->powerSwitchOff();
-				break;
-			case 'power':
-				$data = $device->powerInfo();
-				$this->_printData($data);
-				break;
-			case 'info':
-				$data = $device->deviceInfo();
-				$this->_printData($data);
-				break;
-			case 'status':
-				$data = $device->powerInfo();
-				$this->_printData($data);
-				break;
-			default:
-				echo $this->_consoleOpts->getUsageMessage();
-				exit;
-		}
-//		echo var_dump($data);
-		exit;
-	}
-	
-	protected function _printData($data) {
-		echo "Data\n";
-		echo "====\n";
-		$this->_printDataArray($data);
-		echo "\n";
-	}
-	protected function _printDataArray($data) {
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				echo ucfirst($key) . "\n";
-				echo str_repeat('-', strlen($key)) . "\n";
-				$this->_printDataArray($value);
-				echo "\n";
-			} else {
-				echo " - " . $key . ":\t\t" . $value . "\n";
-			}
-		}
-	}
+
+    /**
+     * 
+     * Returns an object containing the console arguments.
+     * @return Zend_Console_Getopt
+     */
+    public function getConsoleOpts() {
+        // Initialize default console options
+        if (is_null($this->_consoleOpts)) {
+            $this->_consoleOpts = new \Zend_Console_Getopt(
+                array(
+                    'mac|m=s'         => 'Mac address of plugwise device',
+                    'action|a=s'    => 'Action switch-on, switch-off, info, status',
+                    'help|h'        => 'Show help message (this message)',
+                )
+            );
+        }
+        return $this->_consoleOpts;
+    }
+
+
+    /**
+     * 
+     * Sets new console arguments
+     * @param Zend_Console_Getopt $consoleOpts
+     * @return $this
+     */
+    public function setConsoleOpts(Zend_Console_Getopt $consoleOpts = null) {
+        if ($consoleOpts === null) {
+            $consoleOpts = $this->getConsoleOpts();
+        }
+        
+        // Parse Options
+        try {
+            $consoleOpts->parse();
+        } catch (Zend_Console_Getopt_Exception $e) {
+            echo $e->getUsageMessage();
+            exit;
+        }
+        $this->_consoleOpts = $consoleOpts;
+
+        return $this;
+    }
+
+
+    /**
+     * Main method to start the command line tool. 
+     */
+    public function run() {
+        // Set action
+        $mac = $this->_consoleOpts->getOption('mac');
+        $action = $this->_consoleOpts->getOption('action');
+
+        $device = new \PlugWeb\Driver\Device($mac);
+        $data = array();
+        switch($action) {
+            case 'switch-on':
+                $data =$device->powerSwitchOn();
+                break;
+            case 'switch-off':
+                $data = $device->powerSwitchOff();
+                break;
+            case 'power':
+                $data = $device->powerInfo();
+                $this->_printData($data);
+                break;
+            case 'info':
+                $data = $device->deviceInfo();
+                $this->_printData($data);
+                break;
+            case 'status':
+                $data = $device->powerInfo();
+                $this->_printData($data);
+                break;
+            default:
+                echo $this->_consoleOpts->getUsageMessage();
+                exit;
+        }
+        exit;
+    }
+
+
+    /**
+     * Prints an array to STDOUT (using echo)
+     * @param array $data
+     */
+    protected function _printData($data) {
+        echo "Data\n";
+        echo "====\n";
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                echo ucfirst($key) . "\n";
+                echo str_repeat('-', strlen($key)) . "\n";
+                $this->_printDataArray($value);
+                echo "\n";
+            } else {
+                echo " - " . $key . ":\t\t" . $value . "\n";
+            }
+        }
+        echo "\n";
+    }
+
 
 }
--- lib/PlugWeb/Driver/Request.php
+++ lib/PlugWeb/Driver/Request.php
@@ -14,172 +14,185 @@ namespace PlugWeb\Driver;
  *
  */
 class Request {
-	const MAC_PREFIX = '000D6F0000';
-	const ACTION_STICK_INIT = '000A';
-	const ACTION_POWER_INFO = '0012';
-	const ACTION_POWER_SWITCH = '0017';
-	const ACTION_DEVICE_INFO = '0023'; 
-	const ACTION_DEVICE_CALIBRATION = '0026';
-	const ACTION_DEVICE_BUFFER = '0048';
-	const ACTION_CLOCK_INFO = '003E';
-	
-	/**
-	 * 
-	 * Building the request string to initialize the stick
-	 * @return string
-	 */
-	public function actionInitStick() {
-		// 000A
-		$str = self::ACTION_STICK_INIT;
-		return $this->_renderActionString($str);
-	}
 
-	/**
-	 * 
-	 * Build the request string to switch a device on/off
-	 * @param string $mac
-	 * @param boolean $onOff
-	 * @return string
-	 */
-	public function actionPowerSwitch($mac, $switchOnOff = false) {
-		$onOff = ($switchOnOff) ? '01' : '00';
+    const MAC_PREFIX = '000D6F0000';
+    const ACTION_STICK_INIT = '000A';
+    const ACTION_POWER_INFO = '0012';
+    const ACTION_POWER_SWITCH = '0017';
+    const ACTION_DEVICE_INFO = '0023'; 
+    const ACTION_DEVICE_CALIBRATION = '0026';
+    const ACTION_DEVICE_BUFFER = '0048';
+    const ACTION_CLOCK_INFO = '003E';
 
-		// 0017 000D6F0000 729310 01
-		$str = self::ACTION_POWER_SWITCH .
-			self::MAC_PREFIX .$mac .
-			$onOff;
+    /**
+     * 
+     * Building the request string to initialize the stick
+     * @return string
+     */
+    public function actionInitStick() {
+        // 000A
+        $str = self::ACTION_STICK_INIT;
+        return $this->_renderActionString($str);
+    }
 
-		return $this->_renderActionString($str);
-	}
-	
-	/**
-	 * 
-	 * Alias to switch a device on
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionPowerOn($mac) {
-		return $this->actionPowerSwitch($mac, true);
-	}
 
-	/**
-	 * 
-	 * Alias to switch a device off
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionPowerOff($mac) {
-		return $this->actionPowerSwitch($mac, false);
-	}
-	
-	/**
-	 * 
-	 * Build the request string to get the current power usage of a device.
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionPowerInfo($mac) {
-		// 0012 000D6F0000 729310
-		$str = self::ACTION_POWER_INFO .
-			self::MAC_PREFIX .
-			$mac;
+    /**
+     * 
+     * Build the request string to switch a device on/off
+     * @param string $mac
+     * @param boolean $onOff
+     * @return string
+     */
+    public function actionPowerSwitch($mac, $switchOnOff = false) {
+        $onOff = ($switchOnOff) ? '01' : '00';
 
-		return $this->_renderActionString($str);
-	}
+        // 0017 000D6F0000 729310 01
+        $str = self::ACTION_POWER_SWITCH .
+            self::MAC_PREFIX .$mac .
+            $onOff;
 
-	/**
-	 * 
-	 * Build the request string to get the calibration of the device.
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionDeviceCalibration($mac) {
-		// 0012 000D6F0000 729310
-		$str = self::ACTION_DEVICE_CALIBRATION .
-			self::MAC_PREFIX .
-			$mac;
+        return $this->_renderActionString($str);
+    }
 
-		return $this->_renderActionString($str);
-	}
 
-	/**
-	 * 
-	 * Build the request string to get the information of a device.
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionDeviceInfo($mac) {
-		// 0012 000D6F0000 729310
-		$str = self::ACTION_DEVICE_INFO .
-			self::MAC_PREFIX .
-			$mac;
+    /**
+     * 
+     * Alias to switch a device on
+     * @param string $mac
+     * @return string
+     */
+    public function actionPowerOn($mac) {
+        return $this->actionPowerSwitch($mac, true);
+    }
 
-		return $this->_renderActionString($str);
-	}
 
-	/**
-	 * 
-	 * Build the request string to get the buffer of the device.
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionDeviceBuffer($mac, $log) {
-		// 0048 000D6F0000 729310 00044020
-		$str = self::ACTION_DEVICE_BUFFER .
-			self::MAC_PREFIX .
-			$mac .
-			$log;
+    /**
+     * 
+     * Alias to switch a device off
+     * @param string $mac
+     * @return string
+     */
+    public function actionPowerOff($mac) {
+        return $this->actionPowerSwitch($mac, false);
+    }
 
-		return $this->_renderActionString($str);
-	}
 
-	/**
-	 * 
-	 * Returns the clock information of the device. 
-	 * @param string $mac
-	 * @return string
-	 */
-	public function actionClockInfo($mac) {
-		// 003E 000D6F0000 729310
-		$str = self::ACTION_CLOCK_INFO . 
-			self::MAC_PREFIX .
-			$mac;
+    /**
+     * 
+     * Build the request string to get the current power usage of a device.
+     * @param string $mac
+     * @return string
+     */
+    public function actionPowerInfo($mac) {
+        // 0012 000D6F0000 729310
+        $str = self::ACTION_POWER_INFO .
+            self::MAC_PREFIX .
+            $mac;
 
-		return $this->_renderActionString($str);
-	}
-	
-	/**
-	 * 
-	 * Assembles the request string with a header, footer and crc check.
-	 * @param unknown_type $str
-	 * @return string
-	 */
-	protected function _renderActionString($str) {
-		$crc = $this->makeCrcCheckSum($str);
-		$out = $this->_partHeader() .
-			$str .
-			$crc .
-			$this->_partFooter();
-		return $out;
-	}
+        return $this->_renderActionString($str);
+    }
 
-	/**
-	 * 
-	 * Request string header
-	 * @return string
-	 */
-	protected function _partHeader() {
-		return "\x05\x05\x03\x03";
-	}
-	
-	/**
-	 * 
-	 * Request string footer
-	 * @return string
-	 */
-	protected function _partFooter() {
-		return "\x0d\x0a";
-	}
+
+    /**
+     * 
+     * Build the request string to get the calibration of the device.
+     * @param string $mac
+     * @return string
+     */
+    public function actionDeviceCalibration($mac) {
+        // 0012 000D6F0000 729310
+        $str = self::ACTION_DEVICE_CALIBRATION .
+            self::MAC_PREFIX .
+            $mac;
+
+        return $this->_renderActionString($str);
+    }
+
+
+    /**
+     * 
+     * Build the request string to get the information of a device.
+     * @param string $mac
+     * @return string
+     */
+    public function actionDeviceInfo($mac) {
+        // 0012 000D6F0000 729310
+        $str = self::ACTION_DEVICE_INFO .
+            self::MAC_PREFIX .
+            $mac;
+
+        return $this->_renderActionString($str);
+    }
+
+
+    /**
+     * 
+     * Build the request string to get the buffer of the device.
+     * @param string $mac
+     * @return string
+     */
+    public function actionDeviceBuffer($mac, $log) {
+        // 0048 000D6F0000 729310 00044020
+        $str = self::ACTION_DEVICE_BUFFER .
+            self::MAC_PREFIX .
+            $mac .
+            $log;
+
+        return $this->_renderActionString($str);
+    }
+
+
+    /**
+     * 
+     * Returns the clock information of the device. 
+     * @param string $mac
+     * @return string
+     */
+    public function actionClockInfo($mac) {
+        // 003E 000D6F0000 729310
+        $str = self::ACTION_CLOCK_INFO . 
+            self::MAC_PREFIX .
+            $mac;
+
+        return $this->_renderActionString($str);
+    }
+
+
+    /**
+     * 
+     * Assembles the request string with a header, footer and crc check.
+     * @param unknown_type $str
+     * @return string
+     */
+    protected function _renderActionString($str) {
+        $crc = $this->makeCrcCheckSum($str);
+        $out = $this->_partHeader() .
+            $str .
+            $crc .
+            $this->_partFooter();
+        return $out;
+    }
+
+
+    /**
+     * 
+     * Request string header
+     * @return string
+     */
+    protected function _partHeader() {
+        return "\x05\x05\x03\x03";
+    }
+
+
+    /**
+     * 
+     * Request string footer
+     * @return string
+     */
+    protected function _partFooter() {
+        return "\x0d\x0a";
+    }
+
 
     /**
      * 
@@ -197,4 +210,5 @@ class Request {
 
         return strtoupper(dechex($crc));
     }
+
 }
